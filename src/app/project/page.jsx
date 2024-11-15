13487acc46c33ce9fca978fa8e8933c79330c8cc
Abner <1228428816@qq.com>
2024-11-15 19:31:48 +0800

    ✨ feat: 添加项目页面缓存时间设置

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -4,6 +4,9 @@ import { JSDOM } from 'jsdom'
 import createDOMPurify from 'dompurify'
 import React from 'react'
 
+//缓存时间为一小时
+export const revalidate = 3600;
+
 export default async function ProjectPage() {
   const projectPageId = process.env.NOTION_PROJECT_ID
   const md = await fetchMdContent(projectPageId)
